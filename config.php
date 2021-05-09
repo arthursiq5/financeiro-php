ac27c158066e7b1bcc4fc43d159057399200ef5e
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-08 23:34:09 -0300

    Criado arquivo de configurações

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome_sistema = 'Financeiro';
+$url_sistema = 'http://localhost:8888/';
+$email_administrador = 'arthur.onehost@gmail.com';
+
+// DATABASE
+$servidor = '';
+$usuario = '';
+$senha = '';
+$banco = '';
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once('./config.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +14,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
-    <title>Acessar sistema</title>
+    <title><?= $nome_sistema ?></title>
 </head>
 <body class="bg-light">
     <div class="wrapper fadeInDown">

971548d43bab3dfce18ad28e134fc4cf2eed9bc0
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-08 22:52:45 -0300

    Corrigido ícone do projeto

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="icon" href="img/icon.png">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
@@ -19,7 +20,7 @@
     
             <!-- Icon -->
             <div class="fadeIn first">
-                <img src="img/icons8-usuário-de-gênero-neutro-64.png" id="icon" alt="User Icon">
+                <img src="img/icon.png" id="icon" alt="User Icon">
             </div>
     
             <!-- Login Form -->
